baaa924650f66db0123d62c6b5d00042657dd8f9
GPepijn <126915594+GPepijn@users.noreply.github.com>
2023-03-04 00:04:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-const puzzlePieces = document.querySelectorAll('.puzzle-piece');
+// Define variables
+const pieces = document.querySelectorAll('.piece');
+const puzzle = document.querySelector('.puzzle');
+const originalText = document.querySelector('.original-text');
 
-function zoomIn() {
-  const text = this.dataset.text;
-  const puzzle = createPuzzle(text);
-  puzzleContainer.innerHTML = '';
-  puzzleContainer.appendChild(puzzle);
-}
+// Add event listeners to each piece
+pieces.forEach(piece => {
+  piece.addEventListener('click', () => {
+    // Replace the puzzle with a new one
+    puzzle.innerHTML = '';
+    const newPuzzle = createPuzzle(piece.textContent);
+    puzzle.appendChild(newPuzzle);
 
+    // Update the original text
+    originalText.textContent = piece.textContent;
+  });
+});
+
+// Function to create a new puzzle
 function createPuzzle(text) {
+  const words = text.trim().split(' ');
+  const numPieces = words.length;
+
   const puzzle = document.createElement('div');
-  puzzle.classList.add('puzzle-container');
-  const title = document.createElement('h2');
-  title.textContent = text;
-  puzzle.appendChild(title);
-  for (let i = 1; i <= 20; i++) {
+  puzzle.classList.add('puzzle');
+
+  // Create the puzzle pieces
+  for (let i = 0; i < numPieces; i++) {
     const piece = document.createElement('div');
-    piece.classList.add('puzzle-piece');
-    piece.dataset.text = `Stukje ${i}`;
-    piece.textContent = `Tekst voor stukje ${i}`;
-    piece.addEventListener('
+    piece.classList.add('piece');
+    piece.textContent = words[i];
+    puzzle.appendChild(piece);
+  }
+
+  return puzzle;
+}
